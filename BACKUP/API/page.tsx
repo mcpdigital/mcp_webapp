69b21071d472c8b7c53b5862bc773ab75e28e527
mcpdigital <internet@mcpdigital.com>
2023-12-31 19:28:26 -0300

    ll

---
+++ BACKUP/API/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+import FetchApiComponent from "@/components/FetchApiComponent";
+const CallFetch = () => {
+  return <FetchApiComponent />;
+};
+
+export default CallFetch;
---
+++ BACKUP/FetchApiComponent.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+
+// Define the type for the items in the data array
+interface Item {
+  id: number;
+  title: string;
+  // Add other properties as needed
+}
+
+// Use this type for the data prop
+interface DataDisplayProps {
+  data: Item[];
+}
+
+function FetchApiComponent() {
+  const [data, setData] = useState<Item[]>([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
+  if (!data) {
+    return <div>Loading data...</div>;
+  }
+
+  return (
+    <div>
+      <h1>My Data or No Data:</h1>
+      <DataDisplay data={data} />
+    </div>
+  );
+}
+
+const DataDisplay: React.FC<DataDisplayProps> = ({ data }) => {
+  if (!Array.isArray(data)) {
+    return null;
+  }
+
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>{item.title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default FetchApiComponent;
