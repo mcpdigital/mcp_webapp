d918af6cd6f843e6cd4a37fb9548de6684ddfd4c
mcpdigital <internet@mcpdigital.com>
2023-12-31 18:49:48 -0300

    saefasdfsafasfa

--- BACKUP/API/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import React from "react";
-import FetchApiComponent from "@/components/FetchApiComponent";
-const CallFetch = () => {
-  return <FetchApiComponent />;
-};
-
-export default CallFetch;
