cea1da6cdba6774a5bb6948172dc501036c7fc0b
mcpdigital <internet@mcpdigital.com>
2024-01-13 03:53:52 -0300

    delete auth

--- app/(auth)/login/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-import React from "react";
-
-interface loadingProps {}
-
-export const loading: FC<loadingProps> = ({}) => {
-  return <div>loading</div>;
-};
-export default loading;
